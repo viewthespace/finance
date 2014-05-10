dc97b46c30c20cf761e9b90ca4af323b61771399
tubedude <rtrevisan@gmail.com>
2014-05-10 13:41:21 -0300

    Changes in tests

--- test/test_cashflows.rb
+++ test/test_cashflows.rb
@@ -42,7 +42,7 @@ describe "Cashflows" do
     end
 
     it 'should calculate correct rate with new guess (0.1)' do
-      assert_equal '0.11234', @transactions.xirr(0.1).apr.round(5)
+      assert_equal '0.11234'.to_f, @transactions.xirr(0.1).apr.round(5).to_f
     end
 
     it 'should not allow non-numeric guesses' do
